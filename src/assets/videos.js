7b9dde8ddb7a4e2d5fa18de6a9a3c21743195a30
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2022-12-31 15:58:07 +0530

    Add files via upload

---
+++ src/assets/videos.js
@@ -0,0 +1,33 @@
+
+import logo from "./images/logo.png";
+import champAshe from "./images/Champion_ashe_HP.png";
+import champAhri from "./images/Champion_Ahri_HP.png";
+import champGaren from "./images/Champion_garen_HP.png";
+import bg1 from "./images/bg-1.jpg";
+import bg2 from "./images/bg-2.jpg";
+import bg3 from "./images/bg-3.jpg";
+import distortion from "./images/distortion.png";
+
+import cardAshe from "./images/ashe_card.png";
+import cardLux from "./images/lux_card.png";
+import cardAkali from "./images/akali_card.png";
+
+import closeIcon from "./images/two-sword.png";
+
+import trailerImg from "./images/trailer-img.png";
+
+export {
+  logo,
+  champAhri,
+  champAshe,
+  champGaren,
+  bg1,
+  bg2,
+  bg3,
+  distortion,
+  cardAshe,
+  cardLux,
+  cardAkali,
+  closeIcon,
+  trailerImg
+};
